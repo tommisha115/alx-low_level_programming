499e519112a27bb6cfb160ed95eba5f9a30e3e39
Biruk Tesfaye <tommisha115@gmail.com>
2022-10-13 13:14:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-/*
+
+/**
  * main - Printing
  *
  * Return: Always 0 (Successful)
  */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingular puzzle");
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
 	return (0);
+
 }
