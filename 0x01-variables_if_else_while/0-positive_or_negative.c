4a2661567032fcefb229fac8ecf3798c66ee6082
tommisha <113633912+tommisha115@users.noreply.github.com>
2022-10-14 14:26:03 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>                     
 #include <time.h>
-
+#include <string.h>
 /**                                                                        
  * main- Positive anything is better than negative nothing.                
  *
