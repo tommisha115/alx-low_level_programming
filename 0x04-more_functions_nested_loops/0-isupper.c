759755591a456e61bc7797d680fe237e59e11148
tommisha <113633912+tommisha115@users.noreply.github.com>
2022-10-21 02:22:53 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _isdigit - A function that checks for a digit (0 through 9).
- * @c: An input integer
- * Return: 1 if c is a digit or 0 otherwise
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
-	char i = '0';
-	int isdigit = 0;
+	char uppercase = 'A';
+	int isupper = 0;
 
-	for (; i <= '9'; i++)
+	for (; uppercase <= 'Z'; uppercase++)
 	{
-		if (i == c)
+		if (c == uppercase)
 		{
-			isdigit = 1;
+			isupper = 1;
 			break;
 		}
 	}
 
-	return (isdigit);
+	return (isupper);
 }
