6a84a0370d7395fbe7df0ca455a35b6893704ada
Biruk Tesfaye <tommisha115@gmail.com>
2022-10-27 14:46:33 -0700

    0x06 task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,24 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int srclen = 0, src_len = 0;
 
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
+	while (src[index++])
+		src_len++;
 
-	srclen++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-	if (n > srclen)
-		n = srclen;
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	return (dest);
 }
