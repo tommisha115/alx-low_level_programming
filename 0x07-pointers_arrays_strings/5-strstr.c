a9b36ba1845b158ac32f81b8629dbb887a924d89
tommisha <113633912+tommisha115@users.noreply.github.com>
2022-10-31 23:24:04 +0300

    Add files via upload

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,22 +10,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        int index;
-        if (*needle == 0)
-                return (haystack);
-        while (*haystack)
-        {
-                index = 0;
-                if (haystack[index] == needle[index])
-                {
-                        do {
-                                if (needle[index + 1] == '\0')
-                                        return (haystack);
-                                index++;
-                        }
-                        while (haystack[index] == needle[index]);
-                }
-                haystack++;
-        }
-        return ('\0');
+	char *startn = needle, *starth = haystack;
+
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+		haystack = starth + 1;
+	}
+	return (NULL);
 }
